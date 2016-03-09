8edcdf8034b284e6f1c09145710fb64d7f12275f
Odnolap <gelezoo@gmail.com>
2016-03-09 09:16:54 +0300

    HW01 step 2.

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,12 +1,24 @@
 package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * GKislin
  * 11.01.2015.
  */
 public class UserMeal {
+    public static final List<UserMeal> TEST_USER_MEAL_LIST = new ArrayList<>();
+    static {
+        TEST_USER_MEAL_LIST.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+        TEST_USER_MEAL_LIST.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000));
+        TEST_USER_MEAL_LIST.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500));
+        TEST_USER_MEAL_LIST.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000));
+        TEST_USER_MEAL_LIST.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500));
+        TEST_USER_MEAL_LIST.add(new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510));
+    }
     protected final LocalDateTime dateTime;
 
     protected final String description;
--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -1,12 +1,22 @@
 package ru.javawebinar.topjava.model;
 
+import ru.javawebinar.topjava.util.UserMealsUtil;
+
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * GKislin
  * 11.01.2015.
  */
 public class UserMealWithExceed {
+    public static final List<UserMealWithExceed> TEST_UMWE_LIST;
+    static {
+        TEST_UMWE_LIST = UserMealsUtil.getFilteredMealsWithExceeded(UserMeal.TEST_USER_MEAL_LIST, LocalTime.of(0, 0), LocalTime.of(23, 59, 59), 2000);
+    }
+
     protected final LocalDateTime dateTime;
 
     protected final String description;
