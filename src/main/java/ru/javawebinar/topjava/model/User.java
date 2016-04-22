5c536ddf7269b459197cf9ffed23a5549da642a8
Odnolap <gelezoo@gmail.com>
2016-04-22 21:50:05 +0300

    master, patches 7.14 - 7.15

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -78,7 +78,7 @@ public class User extends NamedEntity {
         this.password = password;
         this.caloriesPerDay = caloriesPerDay;
         this.enabled = enabled;
-        this.roles = roles;
+        this.roles = EnumSet.copyOf(roles);
     }
 
     public String getEmail() {
