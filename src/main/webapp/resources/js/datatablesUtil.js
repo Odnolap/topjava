6b0b1c46c41779f4f20cfab87f108e96d3134f10
Odnolap <gelezoo@gmail.com>
2016-05-06 02:32:31 +0300

    master, patches 9.04 - 9.09

--- src/main/webapp/resources/js/datatablesUtil.js
+++ src/main/webapp/resources/js/datatablesUtil.js
@@ -1,5 +1,9 @@
+var form;
+
 function makeEditable() {
-    $('#detailsForm').submit(function () {
+    form = $('#detailsForm');
+
+    form.submit(function () {
         save();
         return false;
     });
@@ -10,8 +14,18 @@ function makeEditable() {
 }
 
 function add() {
+    form.find(":input").val("");
     $('#id').val(0);
     $('#editRow').modal();
+}
+
+function updateRow(id) {
+    $.get(ajaxUrl + id, function (data) {
+        $.each(data, function (key, value) {
+            form.find("input[name='" + key + "']").val(value);
+        });
+        $('#editRow').modal();
+    });
 }
 
 function deleteRow(id) {
@@ -43,7 +57,6 @@ function updateTableByData(data) {
 }
 
 function save() {
-    var form = $('#detailsForm');
     $.ajax({
         type: "POST",
         url: ajaxUrl,
@@ -82,4 +95,18 @@ function failNoty(event, jqXHR, options, jsExc) {
         type: 'error',
         layout: 'bottomRight'
     });
+}
+
+function renderEditBtn(data, type, row) {
+    if (type == 'display') {
+        return '<a class="btn btn-xs btn-primary" onclick="updateRow(' + row.id + ');">Edit</a>';
+    }
+    return data;
+}
+
+function renderDeleteBtn(data, type, row) {
+    if (type == 'display') {
+        return '<a class="btn btn-xs btn-danger" onclick="deleteRow(' + row.id + ');">Delete</a>';
+    }
+    return data;
 }
